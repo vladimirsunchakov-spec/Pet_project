4128e75a7a1bc01d45f0c7d6710cb15bd17a1fef
vladimirsunchakov-spec <vladimirsunchakov@gmail.com>
2026-02-26 12:47:05 +0400

    Revert "Develop"

--- src/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import PostgresDsn
-
-class Settings(BaseSettings):
-    postgres_url: PostgresDsn
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-
-
--- src/models/author_book.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import Table, Column, ForeignKey
-from .base import Base
-
-author_book = Table("author_book",Base.metadata,
-    Column("author_id", ForeignKey("authors.id", ondelete="CASCADE"), primary_key=True),
-    Column("book_id", ForeignKey("books.id", ondelete="CASCADE"), primary_key=True))
