281dd220cdc3ac1b50077e21c38be7aeb943878b
Vladimir Sunchakov <vladimiroff777@rambler.ru>
2026-03-18 13:05:17 +0400

    Убрал индексы

--- src/models/base.py
+++ src/models/base.py
@@ -1,11 +1,21 @@
 import sqlalchemy as sa
-from sqlalchemy.orm import DeclarativeMeta, declarative_base
-
+from sqlalchemy.orm import DeclarativeMeta, declarative_base, Mapped, mapped_column
+from datetime import datetime, timezone
 
 metadata = sa.MetaData()
 
 
 class BaseServiceModel:
+    created_At: Mapped[datetime] = mapped_column(
+        sa.DateTime,
+        default=lambda: datetime.now(timezone.utc)
+    )
+    updated_At: Mapped[datetime] = mapped_column(
+        sa.DateTime,
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc)
+    )
+    is_Deleted: Mapped[bool] = mapped_column(sa.Boolean, default=False)
 
 
     @classmethod
